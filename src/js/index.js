8eb243e8f53ffd539fb86dc3c86193e617803988
joe <dontpanic42k@gmail.com>
2020-12-01 12:22:49 +0500

    final version 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,20 @@
 import '../scss/fonts.scss'
 import '../scss/reset.scss'
+
+
+// мобильное меню
+import '../js/slicknav/jquery.slicknav'
+import '../js/slicknav/slicknav.css'
+$(function(){
+    $('.topbar').slicknav({
+        label: ''
+    });
+});
+
+
 import '../scss/main.scss'
 import '../scss/page-about.scss'
-
 import '../scss/main-media.scss'
+
+import '../scss/menu-mobile.scss'
+
